44854813fcc6c0a79ed15569c4da3631a6906576
Marcos Ruiz Garcia <sobrenombre@gmail.com>
2018-09-20 21:04:24 +0200

    emailConfig.js added without pass

---
+++ routes/restrictedArea/emailConfig.js
@@ -0,0 +1,6 @@
+// Here you have your email and password one in each line
+module.exports = {
+    // Here goes your email
+    EMAIL_SECRET: "youremail@gmail.com",
+    PASS_SECRET: "yourpassword"
+};
