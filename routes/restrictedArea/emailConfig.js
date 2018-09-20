eb0f4e3041c56faa2ae02eed278accfeedcd108b
Marcos Ruiz Garcia <sobrenombre@gmail.com>
2018-09-20 21:17:14 +0200

    fixed untracked files

--- routes/restrictedArea/emailConfig.js
+++
@@ -1,6 +0,0 @@
-// Here you have your email and password one in each line
-module.exports = {
-    // Here goes your email
-    EMAIL_SECRET: "youremail@gmail.com",
-    PASS_SECRET: "yourpassword"
-};
