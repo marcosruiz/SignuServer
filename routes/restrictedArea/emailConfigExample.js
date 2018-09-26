357e9410dec1b013b0e73920255af715da1b6048
Marcos Ruiz Garcia <sobrenombre@gmail.com>
2018-09-26 17:56:52 +0200

    Trying with travis

--- routes/restrictedArea/emailConfigExample.js
+++ routes/restrictedArea/emailConfigExample.js
@@ -1,6 +1,5 @@
-// TODO Rename this file to send emails
 module.exports = {
     // Here goes your email
-    EMAIL_SECRET: "example@gmail.com",
-    PASS_SECRET: "password"
+    EMAIL_SECRET: process.env.EMAIL_SECRET || "example@gmail.com",
+    PASS_SECRET: process.env.PASS_SECRET || "password"
 };
