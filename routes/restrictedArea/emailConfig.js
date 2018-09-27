23c3023d516e3182b9206b228f3a4b8a68ee9d48
Marcos Ruiz Garcia <sobrenombre@gmail.com>
2018-09-27 14:08:17 +0200

    fixed untracked

---
+++ routes/restrictedArea/emailConfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // Here goes your email
+    EMAIL_SECRET: process.env.EMAIL_SECRET || "signu.app@gmail.com",
+    PASS_SECRET: process.env.PASS_SECRET || "penpletiobla7"
+};
